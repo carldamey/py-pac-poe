5efef612190fef94cc8eb3555faeb10beb47a3ad
carldamey <carldamey@gmail.com>
2023-12-04 14:54:57 -0800

    adds board displaying functionality

--- py-pac-poe.py
+++ py-pac-poe.py
@@ -7,9 +7,9 @@ def init():
   global player, board, winner, playing
   print("init called")
   board = [
-  [[], [], [],],
-  [[], [], [],],
-  [[], [], []]
+  [[1], [2], [3],],
+  [[4], [5], [6],],
+  [[7], [8], [9]]
   ]
   player = 1
   winner = None
@@ -20,7 +20,7 @@ def init():
 def game_loop(player, board, winner):
   global playing
   print("gameloop called")
-  show_board()
+  show_board(board)
   
   move = input("Please enter your next move: ".lower())
   if move == "quit":
@@ -32,8 +32,18 @@ def game_loop(player, board, winner):
   player *= -1
   #clear screen (or newline spam)
 
-def show_board():
-  print(board)
+def show_board(board):
+  print(f"""
+      A   B   C
+	
+  1)  {board[0][0]} | {board[0][1]} | {board[0][2]} 
+      -----------
+  2)  {board[1][0]} | {board[1][1]} | {board[1][2]} 
+      -----------
+  3)  {board[2][0]} | {board[2][1]} | {board[2][2]} 
+
+
+  """)
 
 init()
 print("init finished, playing is", playing)
