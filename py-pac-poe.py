0c74d4cba762afeb0ca23953e7d5d25fb188033e
carldamey <carldamey@gmail.com>
2023-12-04 14:47:36 -0800

    implements game looping and quitting

--- py-pac-poe.py
+++ py-pac-poe.py
@@ -1,4 +1,11 @@
+player = None
+board = None
+winner = None
+playing = None
+
 def init():
+  global player, board, winner, playing
+  print("init called")
   board = [
   [[], [], [],],
   [[], [], [],],
@@ -6,21 +13,30 @@ def init():
   ]
   player = 1
   winner = None
-  while True:
-    gameloop()
-  input("Goodbye!")
+  playing = True
+  print()
+  
 
-  def gameloop():
-    #showboard
-    
-    move = lower(input("Please enter your next move: "))
-    if move == "quit": break
-    #checkvalid
-    #claimspace
-    #checkwin
-    player *= 1
-    #clear screen (or newline spam)
+def game_loop(player, board, winner):
+  global playing
+  print("gameloop called")
+  show_board()
+  
+  move = input("Please enter your next move: ".lower())
+  if move == "quit":
+    playing = False
+    return
+  #checkvalid
+  #claimspace
+  #checkwin
+  player *= -1
+  #clear screen (or newline spam)
 
+def show_board():
+  print(board)
 
-
-
+init()
+print("init finished, playing is", playing)
+while playing:
+  game_loop(player, board, winner)
+input("Goodbye!")
