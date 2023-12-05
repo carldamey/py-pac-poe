d8bf04731876e23f8a4928d31652a5307bb26275
carldamey <carldamey@gmail.com>
2023-12-04 20:48:36 -0800

    preps for despaghettification

--- py-pac-poe.py
+++ py-pac-poe.py
@@ -2,6 +2,7 @@ player = None
 board = None
 winner = None
 playing = None
+move = None
 
 def init():
   global player, board, winner, playing
@@ -14,6 +15,7 @@ def init():
   player = 1
   winner = None
   playing = True
+  move = ""
   print()
   
 
@@ -21,12 +23,13 @@ def game_loop(player, board, winner):
   global playing
   print("gameloop called")
   show_board(board)
-  
   move = input("Please enter your next move: ".lower())
   if move == "quit":
     playing = False
     return
-  #checkvalid
+  convert_move()
+  if not check_valid_move(board, move):
+    print("Invalid move!, (find a way to go back to top of gameloop)")
   #claimspace
   #checkwin
   player *= -1
@@ -34,6 +37,8 @@ def game_loop(player, board, winner):
 
 def show_board(board):
   print(f"""
+      {player}'S TURN
+
       A   B   C
 	
   1)  {board[0][0]} | {board[0][1]} | {board[0][2]} 
@@ -42,8 +47,19 @@ def show_board(board):
       -----------
   3)  {board[2][0]} | {board[2][1]} | {board[2][2]} 
 
-
   """)
+
+
+def convert_move():
+  global move
+  move = move.replace("a", "0")
+  move = move.replace("b", "1")
+  move = move.replace("c", "2")
+
+
+def check_valid_move(board, move):
+  if len(move) != 2 or not move.isalnum() or board[move[0][move[1]]]: return False
+  else: return True
 
 init()
 print("init finished, playing is", playing)
